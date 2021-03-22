6ad9cd56019e7afe6698b5a9fbf2bb9421797c39
officialprofile <75314157+officialprofile@users.noreply.github.com>
2021-03-22 12:05:00 +0100

    Updated first automaton

--- automaton1.py
+++ automaton1.py
@@ -1,65 +1,59 @@
 import pygame
 import numpy as np
 
-pygame.init()
-
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
-SCREEN_SIZE = 800
 CELL_SIZE = 20
 MARGIN = 3
-NUM_CELLS = int(SCREEN_SIZE/CELL_SIZE)+2*MARGIN
 NCOL_CELLS = int(SCREEN_WIDTH/CELL_SIZE)+2*MARGIN
 NROW_CELLS = int(SCREEN_HEIGHT/CELL_SIZE)+2*MARGIN
-CELL_BORDER = 1
+CELL_BORDER = 0
 FPS = 10
-BACKGROUND_MUSIC = pygame.mixer.Sound('media/background1.wav')
 
-data = np.random.randint(2, size = (NROW_CELLS, NCOL_CELLS))
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Cellular automaton (Version 1 - Purple Chaos)')
-
-def cell_state(i, j):
-    x = int(np.sqrt(data[i,j]**2+2))
-    return((x%50, x%150, x%150))
-
-def update_states():
-    for i in range(1,NROW_CELLS - 1):
-        for j in range(1, NCOL_CELLS - 1):
-            sum = data[i-1, j] + data[i, i-1] + data[i, j+1] + data[i+1, j] + data[i+1, j+1]
-            #sum = data[i-1, j-1] + data[i-1, j] + data[i-1, j+1] + data[i, i-1] + data[i, j+1] + data[i+1, j-1] + data[i+1, j] + data[i+1, j+1]
-            if sum > 100:
-                data[i, j] -= 1
+def update_states(data):
+    new_generation = np.zeros([NROW_CELLS, NCOL_CELLS])
+    for i in range(2,NROW_CELLS - 2):
+        for j in range(2, NCOL_CELLS - 2):
+            sum = (np.sum(data[i-2:i+3, j-2:j+3]) - data[i, j]) + (np.sum(data[i-1:i+2, j-1:j+2]) - data[i, j])
+            if 0 < sum < 40:
+                new_generation[i, j] = data[i, j] + 2
+            elif data[i, j] > 10 and sum >= 40:
+                new_generation[i, j] = data[i, j] - 1
             else:
-                data[i, j] += 1
+                new_generation[i, j] = 0
+    return new_generation
 
-def draw_cells():
-    screen.fill((0, 0, 0))
-    for i in range(1, NROW_CELLS - 1):
-        for j in range(1, NCOL_CELLS - 1):
+
+def draw_cells(screen, data):
+    for i in range(2, NROW_CELLS - 2):
+        for j in range(2, NCOL_CELLS - 2):
+            cell_color = (int(data[i, j])+50, int(data[i, j]), int(data[i, j])+70)
             cell = pygame.Rect(j*CELL_SIZE-MARGIN*CELL_SIZE, i*CELL_SIZE-MARGIN*CELL_SIZE, CELL_SIZE-CELL_BORDER, CELL_SIZE-CELL_BORDER)
-            pygame.draw.rect(screen, cell_state(i, j), cell)
-
-    pygame.display.update()
-    #generation += 1
+            pygame.draw.rect(screen, cell_color, cell)
 
 
 def main():
-    #generation = 1
+    pygame.init()
+    BACKGROUND_MUSIC = pygame.mixer.Sound('media/background1.wav')
+    #screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Cellular automaton (Version 1 - Purple Chaos)')
     clock = pygame.time.Clock()
-    run = True
-    while run:
+
+    data = np.random.randint(2, size = (NROW_CELLS, NCOL_CELLS))
+
+    while True:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                pygame.quit()
+                return
+        screen.fill((0, 0, 0))
         pygame.mixer.Sound.play(BACKGROUND_MUSIC)
         pygame.mixer.music.stop()
-        update_states()
-        draw_cells()
-
-    pygame.quit()
+        draw_cells(screen, data)
+        data = update_states(data)
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
